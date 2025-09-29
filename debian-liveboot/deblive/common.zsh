3c4c91ef948a4277348c6a6693dae1b23949879a
Christian Iversen <ci@iversenit.dk>
2025-09-29 18:52:24 +0200

    + Add common functions

---
+++ debian-liveboot/deblive/common.zsh
@@ -0,0 +1,43 @@
+autoload -U colors; colors
+
+: ${opt_break:=}
+
+function Log() {
+    local color=$1
+    local mark=$2
+    shift 2
+
+    echo "${fg_bold[blue]}[${color}${mark}${fg_bold[blue]}]${reset_color} $*" > /dev/stderr
+}
+
+function Line() {
+    Log "${fg_bold[white]}" "|" $*
+    # Log "" " " $*
+    # echo " ${fg_bold[green]}${reset_color}  $*" > /dev/stderr
+}
+
+function Debug() {
+    Log "${fg_bold[green]}" "D" $*
+}
+
+function Info() {
+    Log "${fg_bold[green]}" "*" $*
+}
+
+function Warn() {
+    Log "${fg_bold[yellow]}" "W" $*
+}
+
+function Error() {
+    Log "${fg_bold[red]}" "E" $*
+}
+
+function maybe_break() {
+    if [[ ",${opt_break}," == *,$1,* ]]; then
+        Info "-------- Breaking at stage $1 --------"
+        Line "process will continue after shell exit"
+        zsh
+    else
+        Debug "no break at $1";
+    fi
+}
