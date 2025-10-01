084a8c45d14a5dd1af6c8204a262305ad880ef14
Christian Iversen <ci@iversenit.dk>
2025-10-01 23:15:12 +0200

    * Only copy a single kernel/initrd per target

--- debian-liveboot/deblive/generate.sh
+++ debian-liveboot/deblive/generate.sh
@@ -32,23 +32,30 @@ for target in $(layerinfo-get-targets); do
 
     truncate -s0 "${BUILD}/live/${target}.module"
 
+    local KERNEL=
+    local INITRD=
+
     for layer in $(layerinfo-get-layers-for-target $target); do
         echo "${layer}.squashfs" >> "${BUILD}/live/${target}.module"
 
         local INPUT="${LAYERS}/${layer}"
 
-        local KERNEL=(${INPUT}/boot/vmlinuz-*(Nom[1]))
-        [[ -f ${KERNEL} ]] && {
-            cp ${KERNEL} ${BUILD}/${DEST}/vmlinuz
-            Line "Found kernel: ${KERNEL}"
+        local LKERNEL=(${INPUT}/boot/vmlinuz-*(Nom[1]))
+        [[ -f ${LKERNEL} ]] && {
+            KERNEL=$LKERNEL
         }
 
-        local INITRD=(${INPUT}/boot/initrd.img-*(Nom[1]))
-        [[ -f ${INITRD} ]] && {
-            cp ${INITRD} ${BUILD}/${DEST}/initrd
-            Line "Found initrd: ${INITRD}"
+        local LINITRD=(${INPUT}/boot/initrd.img-*(Nom[1]))
+        [[ -f ${LINITRD} ]] && {
+            INITRD=$LINITRD
         }
     done
+
+    cp ${KERNEL} ${BUILD}/${DEST}/vmlinuz
+    Line "  .. kernel: ${KERNEL}"
+
+    cp ${INITRD} ${BUILD}/${DEST}/initrd
+    Line "  .. initrd: ${INITRD}"
 done
 
 Info "Building grub image [bios]"
